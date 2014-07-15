29918c1a960973b94b081bdc38a8e99149d7b7cd
Alberto Cole <acpii2005@gmail.com>
2014-07-14 22:53:29 -0600

    Agregando codigo para imagenes compuestas

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-var Hapi = require('hapi');
-var server = new Hapi.Server(3000);
+var Hapi = require('hapi'),
+  server = new Hapi.Server(3000),
+  https = require('https');
 
+server.route({
+  method: 'GET',
+  path: '/canvasMonge/{urlImagenPerfil}',
+  handler: function (request, reply) {
+    var Canvas = require('canvas');
+    https.get(request.params.urlImagenPerfil, function(res) {
+      var body = '';
+      res.setEncoding('binary');
+      res.on('data', function(chunk) { body += chunk; });
+      res.on('end', function() {
+        var img = new Canvas.Image;
+        img.src = new Buffer(body, 'binary');
+        canvas = new Canvas(img.width,img.height);
+        ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        https.get('https://s3.amazonaws.com/monge/monge-logo.png',function(resLogo){
+          var logoBody = '';
+          resLogo.setEncoding('binary');
+          resLogo.on('data', function(chunk) { logoBody += chunk; });
+          resLogo.on('end', function() {
+            var imgLogo = new Canvas.Image;
+            imgLogo.src = new Buffer(logoBody, 'binary');
+            ctx.drawImage(imgLogo, 0, (img.height-imgLogo.height));
+            reply('<img src="' + canvas.toDataURL() + '" />');
+          })
+        });
+      });
+    });
+  }
+});
 
 server.route({
   method: 'GET',
@@ -9,7 +40,6 @@ server.route({
     reply({
       urlMarca:request.params.urlMarca,
       urlImagenPerfil:request.params.urlImagenPerfil
-
     });
   }
 });
