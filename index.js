34888646df181b9309f4b9e0a53776c53d4d521a
Alberto Cole <acpii2005@gmail.com>
2014-07-14 22:57:25 -0600

    Agregando codigo para imagenes compuestas

--- index.js
+++ index.js
@@ -33,17 +33,6 @@ server.route({
   }
 });
 
-server.route({
-  method: 'GET',
-  path: '/{urlMarca}/{urlImagenPerfil}',
-  handler: function (request, reply) {
-    reply({
-      urlMarca:request.params.urlMarca,
-      urlImagenPerfil:request.params.urlImagenPerfil
-    });
-  }
-});
-
 server.start(function () {
   console.log('Server running at:', server.info.uri);
 });
